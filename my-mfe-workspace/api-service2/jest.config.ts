f31a7c27de04a9f1c2f3a69e566d9a32d3f8c76b
Wassem, Muhammad <waseembt10029@hotmail.com>
2025-08-05 21:57:17 +0200

    api-service2 with fastifiy, Dockerfile, docker-compose

---
+++ my-mfe-workspace/api-service2/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'api-service2',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/api-service2',
+};
